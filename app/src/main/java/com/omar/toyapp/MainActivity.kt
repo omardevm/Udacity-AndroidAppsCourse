8a436cfe8473ca83f1d588f68cfa88d129bb09dd
omardevm <omardguez.contacto@gmail.com>
2021-05-20 11:15:18 -0500

    Second Exercise

--- app/src/main/java/com/omar/toyapp/MainActivity.kt
+++ app/src/main/java/com/omar/toyapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    //Global variable
     private var toyList: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         if (toyList != null) {
             val toyNames: List<String> = ToyBox().getToyNames()
             for (toy: String in toyNames) {
-                toyList!!.append(toy + "\n\n\n")
+                toyList!!.append(toy + "\n\n\n")//append for a series
             }
         }
     }
