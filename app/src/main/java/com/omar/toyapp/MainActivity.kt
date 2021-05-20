e8fc3541d46484001e9778d753d56f719958c149
omardevm <omardguez.contacto@gmail.com>
2021-05-20 11:12:10 -0500

    toy list

--- app/src/main/java/com/omar/toyapp/MainActivity.kt
+++ app/src/main/java/com/omar/toyapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.omar.toyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //Global variable
+    private var toyList: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toyList = findViewById(R.id.tv_toy_names)
+        if (toyList != null) {
+            val toyNames: List<String> = ToyBox().getToyNames()
+            for (toy: String in toyNames) {
+                toyList!!.append(toy + "\n\n\n")
+            }
+        }
     }
 }
